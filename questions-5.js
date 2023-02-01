16dafabacec5fe5bf8c938010dcb9ed84c868a1c
htmllibraryth <119097343+htmllibraryth@users.noreply.github.com>
2023-02-02 05:10:57 +0700

    Add files via upload

---
+++ questions-5.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "คำตอบคือ 1",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "คำตอบคือ 2",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "คำตอบคือ 3",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "คำตอบคือ 4",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 5,
+    question: "คำตอบคือ 1",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 6,
+    question: "คำตอบคือ 2",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 7,
+    question: "คำตอบคือ 3",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 8,
+    question: "คำตอบคือ 4",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 9,
+    question: "คำตอบคือ 1",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 10,
+    question: "คำตอบคือ 2",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+];
