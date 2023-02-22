3cfd7e92982f0c75198930c70a40f4bb0658137b
htmllibraryth <119097343+htmllibraryth@users.noreply.github.com>
2023-02-22 09:32:14 +0700

    Update questions-8.js

--- questions-8.js
+++ questions-8.js
@@ -1,46 +1,46 @@
 let questions = [
     {
     numb: 1,
-    question: "คำตอบคือ 1",
-    answer: "1",
+    question: "CSS ย่อมาจากอะไร?",
+    answer: "Cascading Style Sheets",
     options: [
-      "1",
-      "2",
-      "3",
-      "4"
+      "Cascading Status Sheets",
+      "Cascading Stoke Sheets",
+      "Cascading Style Sheets",
+      "Cascading Store Shhets"
     ]
   },
     {
     numb: 2,
-    question: "คำตอบคือ 2",
-    answer: "2",
+    question: "CSS มีกี่รูปแบบ",
+    answer: "3 รูปแบบ",
     options: [
-      "1",
-      "2",
-      "3",
-      "4"
+      "1 รูปแบบ",
+      "2 รูปแบบ",
+      "3 รูปแบบ",
+      "4 รูปแบบ"
     ]
   },
     {
     numb: 3,
-    question: "คำตอบคือ 3",
-    answer: "3",
+    question: "CSS มี 3 รูปแบบมีอะไรบ้าง",
+    answer: "Inline ,Embed ,External",
     options: [
-      "1",
-      "2",
-      "3",
-      "4"
+      "Inline ,Embed ,External",
+      "Internal ,Centernal ,External",
+      "Inline ,Centernal ,External",
+      "ไม่มีข้อถูก"
     ]
   },
     {
     numb: 4,
-    question: "คำตอบคือ 4",
-    answer: "4",
+    question: "การใช้ CSS รูปแบบใดแทรกในลิงค์",
+    answer: "Inline",
     options: [
-      "1",
-      "2",
-      "3",
-      "4"
+      "Inline",
+      "Embed",
+      "Externl",
+      "ไม่มีรูปแบบดังกล่าว"
     ]
   },
     {
